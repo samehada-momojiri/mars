ef0e915435a44a54e07f6e29058d86f86d923102
WORLD\yusuk <yusuke.harada.007@gmail.com>
2026-04-18 00:00:42 +0900

    created a project by Spring Initializr

---
+++ src/test/java/com/nine2nine/app/mars/MarsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nine2nine.app.mars;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
